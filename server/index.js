f456ed4c0cb4bc6bc72f28d7433e5b5444f419b7
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-09 16:01:46 -0800

    server starter code

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json()); // this will allow json as the input of the backend
+app.use(cookieParser()); // this will allow us to extract the cookie from the browser without any problem
+
+dotenv.config({ path: "../.env" });
+
+const mongo_url = process.env.MONGO;
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
