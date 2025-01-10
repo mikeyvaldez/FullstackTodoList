a1d4a707c8ffb3b61fbd84fcea867ec12550cbed
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-10 13:01:35 -0800

    added redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+
+
+// the provider and store are related to redux
+// redux is used to retain the global state of a user, for instance.
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
