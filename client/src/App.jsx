4346c5aeb4dd882508514ffa978f5b801bdc2759
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-10 11:39:49 -0800

    created and added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 // here we utilize the react router dom imports to be able to route to our pages
 
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,83 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap font-bold dark:text-white text-lg sm: text-xl"
+            >
+              <span className="px-2 py-1 text-3xl text-blue-500">GetterDone!</span>
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Owner" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="www.<myportfolio website url>.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Michael Valdez
+                </Footer.Link>                
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/mikeyvaldez"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/michael-valdez-a29179260/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/michael-valdez-a29179260/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Terms &amp; Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="GetterDone!"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">                        
+            <Footer.Icon href="https://github.com/mikeyvaldez" icon={BsGithub} />
+            <Footer.Icon href="https://www.linkedin.com/in/michael-valdez-a29179260/" icon={BsLinkedin} />
+            
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
