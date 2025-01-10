fb6ee3314898338c71a9cc1fe4c219e9005320a2
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-09 16:47:12 -0800

    create pages and routes

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">About GetItDone!</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              GetItDone! is a blog website that I created to help me understand
+              web development. I would love to become a Full stack web developer
+              some day and by creating this website, it shows how motivated and
+              committed I am to this profession. I hope you enjoy my blog
+              website.
+            </p>
+
+            <p>
+              Michael is always learning and exploring new technologies, so be sure to check back with him!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
