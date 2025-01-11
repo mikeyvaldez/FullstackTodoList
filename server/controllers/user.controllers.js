5058fdd08927edbe4a54fc826e7fe137907f7f15
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-10 16:25:50 -0800

    complete delete functionality

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -3,9 +3,13 @@ import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js"
 
 
+// -----Test-------------------------
 export const test = (req, res) => {
     return res.json({ message: "Api is working!" })
 }
+
+// ------------end of test------------------
+
 
 //-------UPDATE USER--------------
 export const updateUser = async (req, res, next) => {
@@ -57,3 +61,88 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+// -----------------End of Update user--------------------------------------------------------
+
+// --------------------DELETE USER-------------------------------------------------------------
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+// ------------------------End of Delete User--------------------------------------------------------
+
+
+
+
+// export const signout = (req, res, next) => {
+//   try {
+//     res
+//       .clearCookie("access_token")
+//       .status(200)
+//       .json("User has been signed out");
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// export const getUsers = async (req, res, next) => {
+//   if (!req.user.isAdmin) {
+//     return next(errorHandler(403, "You are not allowed to see all users"));
+//   }
+
+//   try {
+//     const startIndex = parseInt(req.query.startIndex) || 0;
+//     const limit = parseInt(req.query.limit) || 9;
+//     const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+//     const users = await User.find()
+//       .sort({ createdAt: sortDirection })
+//       .skip(startIndex)
+//       .limit(limit);
+
+//       const securedUser = users.map((user) => {
+//         const { password, ...rest } = user._doc;
+//         return rest;
+//       });
+
+//       const totalUsers = await User.countDocuments();
+//       const now = new Date();
+//       const oneMonthAgo = new Date(
+//         now.getFullYear(),
+//         now.getMonth() - 1,
+//         now.getDate()
+//       );
+
+//       const lastMontUsers = await User.countDocuments({
+//         createdAt: { $gte: oneMonthAgo },
+//       });
+
+//       res.status(200).json({
+//         users: securedUser,
+//         totalUsers,
+//         lastMontUsers,
+//       })
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// export const getUser = async (req, res, next) => {
+//   try {
+//     const user = await User.findById(req.params.userId);
+//     if (!user) {
+//       return next(errorHandler(404, 'User not found'));
+//     }
+//     const { password, ...rest } = user._doc;
+//     res.status(200).json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controllers.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controllers.js";
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router();
 
 router.get("/test", test); // this gets used in the index file and looks like this in the browser search (http://localhost:[port]/api/user/test)
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
